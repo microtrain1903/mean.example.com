d81af7d662fb390f6918ca9df382fb49c14d6223
CHAD-SVASTISALEE <swasgrafx@gmail.com>
2019-04-26 11:18:58 -0500

    Edited index.pug

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  console.log(req.session);
-  res.send('Welcome to MicroTrain1903');
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Welcome MicroTrain1903',
+    name: 'Chad'
+  });
 });
 router.get('/logout', function(req, res){
   console.log(req.session);
